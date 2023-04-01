657f39f08fde38bad7ccf337366c9f20b9237465
Ricardo Guimarães <rfguimaraes0@gmail.com>
2023-04-01 11:33:29 +0200

    Configure nox lint session with ruff

--- noxfile.py
+++ noxfile.py
@@ -5,8 +5,18 @@
 import nox
 
 
+locations = "src", "tests", "noxfile.py"
+
+
 @nox.session(python=["3.9"])
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session(python=["3.9"])
+def lint(session):
+    args = session.posargs or locations
+    session.run("poetry", "install", external=True)
+    session.run("ruff", "check", *args)
