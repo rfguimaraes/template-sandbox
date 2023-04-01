6f3d227c3cc5c1c345b2ce0e3497ef996e703ff6
Ricardo Guimarães <rfguimaraes0@gmail.com>
2023-04-01 09:56:39 +0200

    Add basic "Hello World" script

---
+++ src/template_sandbox/greeter.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023 The Template-Sandbox Authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from . import __version__
+
+
+def main():
+    print(f"Hello, World! ({__version__})")
