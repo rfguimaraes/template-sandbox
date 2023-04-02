02df8ad24096b777e1a7f8a8f17847fecbd34d1e
Ricardo Guimarães <rfguimaraes0@gmail.com>
2023-04-02 14:39:17 +0200

    Fix ruff issues in noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -2,12 +2,19 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+"""Definition of nox sessions (tasks)."""
+
 import tempfile
 import nox
 from poetry.factory import Factory
 
 
-def install_on_nox_from_poetry_lock(session, poetry_args, *args, **kwargs):
+def _install_on_nox_from_poetry_lock(
+    session: nox.Session,
+    poetry_args: tuple,
+    *args: list,
+    **kwargs: dict,
+) -> None:
     with tempfile.NamedTemporaryFile() as constraints:
         session.run(
             "poetry",
@@ -26,33 +33,37 @@ nox.options.sessions = "lint", "safety", "tests", "type"
 
 
 @nox.session(python=["3.9"])
-def tests(session):
+def tests(session: nox.Session) -> None:
+    """Run tests."""
     args = session.posargs or ["--cov", "-m", "not e2e"]
     config = Factory().create_poetry()
     dependencies = config.package.dependency_group("test").dependencies
-    names = list(map(lambda x: x.name, dependencies))
+    names = [x.name for x in dependencies]
     names.remove("nox")
-    install_on_nox_from_poetry_lock(session, ("--with", "test"), *names)
+    _install_on_nox_from_poetry_lock(session, ("--with", "test"), *names)
     session.run("poetry", "install", "--only", "main", external=True)
     session.run("pytest", *args)
 
 
 @nox.session(python=["3.9"])
-def lint(session):
+def lint(session: nox.Session) -> None:
+    """Lint check with ruff."""
     args = session.posargs or locations
-    install_on_nox_from_poetry_lock(session, ("--only", "lint"), "ruff")
+    _install_on_nox_from_poetry_lock(session, ("--only", "lint"), "ruff")
     session.run("ruff", "check", *args)
 
 
 @nox.session(python=["3.9"])
-def black(session):
+def black(session: nox.Session) -> None:
+    """Format with black."""
     args = session.posargs or locations
-    install_on_nox_from_poetry_lock(session, ("--only", "lint"), "black")
+    _install_on_nox_from_poetry_lock(session, ("--only", "lint"), "black")
     session.run("black", *args)
 
 
 @nox.session(python=["3.9"])
-def safety(session):
+def safety(session: nox.Session) -> None:
+    """Safety check with public repository."""
     with tempfile.NamedTemporaryFile() as requirements:
         session.run(
             "poetry",
@@ -63,16 +74,17 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        install_on_nox_from_poetry_lock(session, ("--only", "lint"), "safety")
+        _install_on_nox_from_poetry_lock(session, ("--only", "lint"), "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 @nox.session(python=["3.9"])
-def mypy(session):
+def mypy(session: nox.Session) -> None:
+    """Type check with mypy."""
     args = session.posargs or locations
     # Workaround the need of poetry inside the nox session.
     # It is easier to skip this file from mypy check.
     args = tuple([x for x in args if x != "noxfile.py"])
-    install_on_nox_from_poetry_lock(session, ("--with", "type"), "mypy")
+    _install_on_nox_from_poetry_lock(session, ("--with", "type"), "mypy")
     session.run("poetry", "install", "--only", "main", external=True)
     session.run("mypy", *args)
