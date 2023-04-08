a81be81b36e8603d8107806dad39af39a9d072c4
Ricardo Guimarães <rfguimaraes0@gmail.com>
2023-04-08 14:06:25 +0200

    Setup hypothesis
    Also change mypy session so it passes correctly

--- src/template_sandbox/greeter.py
+++ src/template_sandbox/greeter.py
@@ -10,3 +10,21 @@ from . import __version__
 def main() -> None:
     """Print the greeting message and version number."""
     print(f"Hello, World! ({__version__})")
+
+
+def message_to_shout(message: str) -> str:
+    """
+    Greet a person by name.
+
+    :param message: The message to convert to "shout"
+    :type message: str
+    :return: The shout-formatted message if the string is not empty,
+    empty string otherwise
+    :rtype: str
+    """
+    if not message:
+        return ""
+    new_message = message.upper()
+    if new_message[-1] != "!":
+        new_message = new_message + "!"
+    return new_message
--- tests/test_greeter.py
+++ tests/test_greeter.py
@@ -5,6 +5,8 @@
 """Placeholder test for the main placeholder."""
 
 import pytest
+from hypothesis import example, given
+from hypothesis.strategies import text
 from template_sandbox import __version__, greeter
 
 
@@ -19,3 +21,19 @@ def test_main_greeter_correct_greeting(capsys: pytest.CaptureFixture[str]) -> No
     captured = capsys.readouterr()
     assert captured.out == f"Hello, World! ({__version__})\n"
     assert not captured.err
+
+
+@given(text())
+@example("")
+def test_shout_ensures_exclamation_mark(message: str) -> None:
+    """
+    Test the converter of message to shout-like string.
+
+    Test that the message has an exclamation mark at the end unless it is empty.
+
+    :param message: the message to be converted
+    :type message: the message to be converted
+    :returns: None
+    """
+    shout = greeter.message_to_shout(message)
+    assert not shout or shout[-1] == "!"
